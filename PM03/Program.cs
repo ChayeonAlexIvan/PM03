e1bbf02c60e9e494bcd3e46e010f4c0b62e1d806
stu-pksp118 <stu-pksp118@hq.corp.vlsu.ru>
2022-04-19 09:30:41 +0300

    Добавьте файлы проекта.

---
+++ PM03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PM03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

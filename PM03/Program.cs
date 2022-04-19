b7a72b5d1e505a8cb638ef62ee3a5780c79ab99e
stu-pksp118 <stu-pksp118@AMDVLA3511GW12>
2022-04-19 09:39:40 +0300

    Сохранение параметров класса

--- PM03/Program.cs
+++ PM03/Program.cs
@@ -7,6 +7,18 @@ namespace PM03
         public string Name;
         public string Manufacturer;
         public int Price;
+        public void SetName(string Name)
+        {
+            this.Name = Name;
+        }
+        public void SetManufacter(string Manufacturer)
+        {
+            this.Manufacturer = Manufacturer;
+        }
+        public void SetPrice(int Price)
+        {
+            this.Price = Price;
+        }
     }
     class Program
     {

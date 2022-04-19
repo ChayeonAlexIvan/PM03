3d988af301efcd378c13d1938a343fd83a5305d2
stu-pksp118 <stu-pksp118@AMDVLA3511GW12>
2022-04-19 09:33:43 +0300

    Создание класса

--- PM03/Program.cs
+++ PM03/Program.cs
@@ -2,6 +2,12 @@
 
 namespace PM03
 {
+    public class App
+    {
+        public string Name;
+        public string Manufacturer;
+        public int Price;
+    }
     class Program
     {
         static void Main(string[] args)
